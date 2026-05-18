27305c18850d4d8c733b458457c927e49850a181
JoySutradhar <joysutradhar@joinventureai.com>
2026-05-19 00:04:28 +0600

    feat: problem 4 is solved

---
+++ Problem_4.ts
@@ -0,0 +1,17 @@
+type PropsType= {
+    title: string;
+    rating: number;
+}
+
+const filterByRating =  (items: PropsType[]): PropsType[]=> {
+    const CopyArry = [...items] // for immutability of the original array now the copy of the array will be used
+    return CopyArry.filter(item => item.rating >= 4);
+};
+
+const books: PropsType[] = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
+
+console.log(filterByRating(books));
